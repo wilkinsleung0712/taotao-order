7ecb9d196aafb651dac2e984a88184d1ed4020fc
wilkinsleung0712 <wilkinsweiqiangliang@gmail.com>
2017-03-05 23:00:54 +1100

    adding jedis configuration and utils

---
+++ src/main/java/com/taotao/order/dao/JedisClient.java
@@ -0,0 +1,23 @@
+package com.taotao.order.dao;
+
+public interface JedisClient {
+
+    public String set(String key, String value);
+
+    public String get(String key);
+
+    public long hset(String hkey, String key, String value);
+
+    public String hget(String hkey, String key);
+
+    public long hdel(String hkey, String key);
+
+    public long incr(String key);
+
+    public long expire(String key, long expireTimeInSecond);
+
+    public long ttl(String key);
+    
+    public long del(String key);
+
+}
